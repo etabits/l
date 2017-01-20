b8d970007d45d4da9f8dc2bbdc92c2bbf368b796
Hasan Arous <arous.hasan@gmail.com>
2017-01-20 20:10:41 +0200

    simplify segments and fix some stuff

--- src/MixedPipe.js
+++ src/MixedPipe.js
@@ -1,77 +1,87 @@
 'use strict';
 
-function debug() {
-  console.log.apply(console,arguments)  
-}
 class MixedPipe {
 
-  constructor(pipeline) {
-    this.pipeline = pipeline;
+  constructor(segments) {
+    this.segments = segments;
   }
 
   execute(value) {
-    debug('executing on', value)
+    this.log('executing on', value)
     this.value = value;
     this.step = 0;
     this.next()
   }
 
   next () {
-    var step = this.pipeline[this.step];
     var self = this;
+    var step = this.segments[this.step];
     if (!step) {
-      debug('finished!', this.value)
+      self.log('finished!', this.value);
       return;
     }
-    debug(this.step, step.name, this.value)
-    if ('promise'==step.type) {
-      step.func(this.value)
-      .then(function(newValue) {
-        debug('\tpromise ret', newValue)
-        self.value = newValue;
-        self.step++
-        self.next()
-      })
-    } else if ('async'==step.type) {
-      step.func(this.value, function(err, newValue) {
-        debug('\tasync ret', newValue)
-        self.value = newValue
-        self.step++;
-        self.next()
-
-      })
-    } else if ('stream'==step.type) {
-      var firstStream = step.func();
+    self.log(this.step, step.name || 'anon', this.value);
+    if (step.stream) {
+      var firstStream = step.stream();
       var lastStream = firstStream;
       for (var streamsEnd = this.step + 1;
-        this.pipeline[streamsEnd] && 'stream'==this.pipeline[streamsEnd].type;
+        this.segments[streamsEnd] && this.segments[streamsEnd].stream;
         ++streamsEnd) {
-        var streamStep = this.pipeline[streamsEnd];
-        var anotherStream = streamStep.func();
+        var streamStep = this.segments[streamsEnd];
+        var anotherStream = streamStep.stream();
         lastStream.pipe(anotherStream)
         lastStream = anotherStream;
-        console.log('found another stream at ', streamsEnd, 'name', streamStep.name)
+        self.log('found another stream at ', streamsEnd, 'name', streamStep.name)
       }
 
       var buf;
       lastStream.on('data', function(data) {
         if (!buf) {
-          buf = Buffer.from(data)
+          buf = Buffer.from(data);
         } else {
-          buf += data;
+          buf = Buffer.concat([buf, data]);
         }
       })
       lastStream.on('end', function() {
         self.value = buf;
-        debug('\tstream end', self.value.toString('hex'))
-        self.step+=2;
+        self.log('\tstream end', self.value.toString('hex'))
+        self.step+=(streamsEnd-self.step);
         self.next();
       })
       firstStream.write(this.value);
       firstStream.end();
     } else {
-      console.error('unknown type', step.type)
+      var ret = step(this.value, function(err, newValue) {
+        self.log('\tasync ret', newValue)
+        self.value = newValue
+        self.step++;
+        self.next();
+      });
+      // console.log(self.step, ret)
+      if ('undefined'==typeof ret) {
+        // it was async, do nothing!
+      } else if (ret instanceof Promise) {
+        ret.then(function(newValue) {
+          self.log('\tpromise ret', newValue)
+          self.value = newValue;
+          self.step++;
+          self.next();
+        });
+
+      } else {
+        self.log('\tdirect result', ret)
+        self.value = ret
+        self.step++;
+        self.next();
+
+      }
     }
+    return;
+  }
+
+  log() {
+    if (!this.debug) return;
+    console.log.apply(console,arguments)
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -4,44 +4,20 @@ const crypto = require('crypto');
 var MixedPipe = require('../src/MixedPipe');
 
 var pe = new MixedPipe([
+  (val)=> val*2,
+  (val)=> Promise.resolve(val*3),
+  function add4(val, done) {
+    setTimeout(function() {
+      done(null, val+4)
+    },1)
+  },
+  (val)=>'' + val,
   {
-    name: 'mul3',
-    type: 'promise',
-    func: function (value) {
-      return Promise.resolve(value*3);
-    }
+    stream: ()=>crypto.createHash('sha1')
   },
   {
-    name: 'add9',
-    type: 'async',
-    func: function (value, done) {
-      setTimeout(function() {
-        done(null, value+9)
-      },1)
-    }
+    stream: ()=>crypto.createHash('md5')
   },
-  {
-    name: 'toString',
-    type: 'promise',
-    func: function (value) {
-      return Promise.resolve(''+value);
-    }
-  },
-  {
-    name: 'sha1',
-    type: 'stream',
-    func: ()=>crypto.createHash('sha1')
-  },
-  // {
-  //  name: 'hexify',
-  //  type: 'promise',
-  //  func: (value)=>Promise.resolve(value.toString('hex'))
-  // },
-  {
-    name: 'md5',
-    type: 'stream',
-    func: ()=>crypto.createHash('md5')
-  },
-
 ]);
+pe.debug = true;
 pe.execute(5)
