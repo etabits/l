d3acd6474dc3d23fe050263c7a41e0d1f719b476
Hasan Arous <arous.hasan@gmail.com>
2017-06-18 02:29:15 +0300

    allow for shorter call syntax when using wrapper

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const Line = require('./src/Line')
 
 function wrapper (segments) {
+  if (arguments.length > 1) {
+    segments = Array.from(arguments)
+  } else if (!Array.isArray(segments)) {
+    segments = [segments]
+  }
   var line = new Line(segments)
   return function () {
     return line.execute.apply(line, arguments)
--- test/call-as-function.js
+++ test/call-as-function.js
@@ -1,19 +1,42 @@
 'use strict'
 import test from 'ava'
 
-const line = require('../')
+const l = require('../')
 
 test('call as function', t => {
   t.plan(1)
-  const l = line([
+  return l([
     {
       $type: 'async',
       func: (value, done) => setTimeout(() => done(null, value * 3), 1)
     },
     (val) => Promise.resolve(val * 2),
     (val) => val * 7
-  ])
-  return l(1).then(function (result) {
+  ])(1).then(function (result) {
+    t.is(result, 42)
+  })
+})
+
+test('call without an array', t => {
+  t.plan(1)
+  return l({
+    one: Promise.resolve(1),
+    two: (num) => num / 4
+  })(8).then(function (result) {
+    t.deepEqual(result, {
+      one: 1,
+      two: 2
+    })
+  })
+})
+
+test('call multiple arguments', t => {
+  t.plan(1)
+  return l(
+    (num) => num * 2,
+    (num) => Promise.resolve(num * 3),
+    (num, done) => done(null, num * 7)
+  )(1).then(function (result) {
     t.is(result, 42)
   })
 })
